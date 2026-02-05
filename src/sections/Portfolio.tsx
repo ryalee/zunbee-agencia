f5c0e596087047640573a36eaf6e3da5589facad
Ryan <sodre100ryan@gmail.com>
2026-02-05 10:31:58 -0300

    Change import of projects from .js to .ts

--- src/sections/Portfolio.tsx
+++ src/sections/Portfolio.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react';
-import { projects } from '../data/projects.js';
+import { projects } from '../data/projects.ts';
 import Image from 'next/image'
 
 type Project = {
